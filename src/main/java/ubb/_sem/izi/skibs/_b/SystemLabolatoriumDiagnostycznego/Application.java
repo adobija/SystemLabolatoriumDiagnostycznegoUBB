018c0bf5abb02dcfb09e39d8245687e197098be0
adobija <a.dobija2245@gmail.com>
2026-03-16 17:53:57 +0100

    Add all project files generated

---
+++ src/main/java/ubb/_sem/izi/skibs/_b/SystemLabolatoriumDiagnostycznego/Application.java
@@ -0,0 +1,13 @@
+package ubb._sem.izi.skibs._b.SystemLabolatoriumDiagnostycznego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
