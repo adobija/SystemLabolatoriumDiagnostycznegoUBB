b36ed2647cad999de4cead1329056b96c33c42be
JCode <kruzlakjakub6@gmail.com>
2026-05-06 20:23:16 +0200

    stworzenie entity StatusZlecenia

---
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/entity/StatusZlecenia.java
@@ -0,0 +1,34 @@
+package ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "StatusZlecenia")
+public class StatusZlecenia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_status_zlecenia")
+    private Long id;
+
+    @Column(name = "kod", nullable = false, unique = true, length = 20)
+    private String kod;
+
+    @Column(name = "opis", nullable = false, length = 100)
+    private String opis;
+
+    public StatusZlecenia() {}
+
+    public StatusZlecenia(String kod, String opis) {
+        this.kod = kod;
+        this.opis = opis;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getKod() { return kod; }
+    public void setKod(String kod) { this.kod = kod; }
+
+    public String getOpis() { return opis; }
+    public void setOpis(String opis) { this.opis = opis; }
+}
