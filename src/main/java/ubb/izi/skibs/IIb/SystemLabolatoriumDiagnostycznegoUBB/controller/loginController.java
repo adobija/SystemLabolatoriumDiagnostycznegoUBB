a73f197de99970d0d0c7972fe20a7870e29dd775
adobija <a.dobija2245@gmail.com>
2026-05-28 21:03:09 +0200

    Update verifaction of logged user

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/controller/loginController.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/controller/loginController.java
@@ -1,5 +1,6 @@
 package ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,12 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class loginController
 {
     @GetMapping("/login")
-    public String showPanelLogowania() {
+    public String showPanelLogowania(Authentication authentication) {
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getName())) {
+            return "redirect:/dashboard";
+        }
         return "Template/Logowanie";
     }
 
     @GetMapping("/register")
-    public String showPanelRejestracji() {
+    public String showPanelRejestracji(Authentication authentication) {
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getName())) {
+            return "redirect:/dashboard";
+        }
         return "Template/Rejestracja";
     }
 }
