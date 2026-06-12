f72f328b68f27e0468fe2efeba23cd078c1a81c0
adobija <a.dobija2245@gmail.com>
2026-06-12 19:57:51 +0200

    Create constructors and getters and setters

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/entity/Pacjent.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/entity/Pacjent.java
@@ -28,7 +28,7 @@ public class Pacjent {
     @Column(length = 20)
     private String telefon;
 
-    @Column(name = "data_urodzenia", nullable = false)
+    @Column(name = "data_urodzenia")
     private LocalDate dataUrodzenia;
 
     @ManyToOne
@@ -44,4 +44,91 @@ public class Pacjent {
     public Pacjent() {}
 
     // gettery/settery
+
+    public Pacjent(String imie, String nazwisko, String pesel, Plec plec) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.pesel = pesel;
+        this.plec = plec;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Uzytkownik getUzytkownik() {
+        return uzytkownik;
+    }
+
+    public void setUzytkownik(Uzytkownik uzytkownik) {
+        this.uzytkownik = uzytkownik;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public LocalDate getDataUrodzenia() {
+        return dataUrodzenia;
+    }
+
+    public void setDataUrodzenia(LocalDate dataUrodzenia) {
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    public Plec getPlec() {
+        return plec;
+    }
+
+    public void setPlec(Plec plec) {
+        this.plec = plec;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getNumerKartyPacjenta() {
+        return numerKartyPacjenta;
+    }
+
+    public void setNumerKartyPacjenta(String numerKartyPacjenta) {
+        this.numerKartyPacjenta = numerKartyPacjenta;
+    }
 }
