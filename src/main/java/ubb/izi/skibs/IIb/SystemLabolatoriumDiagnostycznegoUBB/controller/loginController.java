839f59c08b9d3a06bbbe40c9a9714f5a986ea5d7
adobija <a.dobija2245@gmail.com>
2026-06-12 20:47:33 +0200

    Change error codes

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/controller/loginController.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/controller/loginController.java
@@ -1,10 +1,12 @@
 package ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,7 @@ import ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.repository.PlecRep
 import ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.repository.RolaRepo;
 import ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.repository.UzytkownikRepo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -67,16 +70,32 @@ public class loginController
                                  @RequestParam("nazwisko") String nazwisko,
                                  @RequestParam("pesel") String pesel,
                                  @RequestParam("plec") String plec,
+                                 @RequestParam("telefon") String telefon,
+                                 @RequestParam("dataUrodzenia") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataUrodzenia,
+                                 @RequestParam("adres") String adres,
                                  @RequestParam("email") String email,
-                                 @RequestParam("haslo") String haslo) {
+                                 @RequestParam("haslo") String haslo,
+                                 Model model) {
+
         if (authentication != null
                 && authentication.isAuthenticated()
                 && !"anonymousUser".equals(authentication.getName())) {
             return "redirect:/dashboard";
         }
         if (uzytkownikRepo.existsByEmail(email)) {
-            return "redirect:/register?error=email_exists";
+            model.addAttribute("status", 400);
+            model.addAttribute("title", "Taki e-mail już istnieje");
+            model.addAttribute("message", "Użytkownik z podanym adresem e-mail już istnieje w systemie.");
+            return "error";
         }
+
+        if (pacjentRepo.existsByPesel(pesel)) {
+            model.addAttribute("status", 400);
+            model.addAttribute("title", "Taki PESEL już istnieje");
+            model.addAttribute("message", "Pacjent z podanym numerem PESEL już istnieje w systemie.");
+            return "error";
+        }
+
 
         Uzytkownik uzytkownik = new Uzytkownik(email, passwordEncoder().encode(haslo));
         Rola rola = rolaRepo.findById(3L)
@@ -91,6 +110,9 @@ public class loginController
         Pacjent pacjent = new Pacjent(imie, nazwisko, pesel, plecDlaPacjent);
         pacjent.setNumerKartyPacjenta("KARTA00"+ nowoDodany.getId());
         pacjent.setUzytkownik(nowoDodany);
+        pacjent.setTelefon(telefon);
+        pacjent.setDataUrodzenia(dataUrodzenia);
+        pacjent.setAdres(adres);
 
         //zapisanie pacjenta do bazy
         pacjentRepo.save(pacjent);
