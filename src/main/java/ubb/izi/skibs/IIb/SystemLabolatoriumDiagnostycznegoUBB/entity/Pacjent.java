198e2800ba0f5b91e324e6605615199988a7af51
1x4mil <kamilp200316@gmail.com>
2026-06-12 22:36:47 +0200

    Stworzenie Pacjent java

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/entity/Pacjent.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/entity/Pacjent.java
@@ -41,7 +41,86 @@ public class Pacjent {
     @Column(name = "numer_karty_pacjenta", length = 50)
     private String numerKartyPacjenta;
 
-    public Pacjent() {}
+    public Pacjent() {
+    }
 
-    // gettery/settery
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Uzytkownik getUzytkownik() {
+        return uzytkownik;
+    }
+
+    public void setUzytkownik(Uzytkownik uzytkownik) {
+        this.uzytkownik = uzytkownik;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public LocalDate getDataUrodzenia() {
+        return dataUrodzenia;
+    }
+
+    public void setDataUrodzenia(LocalDate dataUrodzenia) {
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    public Plec getPlec() {
+        return plec;
+    }
+
+    public void setPlec(Plec plec) {
+        this.plec = plec;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getNumerKartyPacjenta() {
+        return numerKartyPacjenta;
+    }
+
+    public void setNumerKartyPacjenta(String numerKartyPacjenta) {
+        this.numerKartyPacjenta = numerKartyPacjenta;
+    }
 }
