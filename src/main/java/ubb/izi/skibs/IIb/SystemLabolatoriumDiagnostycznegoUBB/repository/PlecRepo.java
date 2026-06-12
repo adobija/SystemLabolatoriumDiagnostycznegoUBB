b50f7ebfdf49e9c278b4f7e75ec41e892217f641
adobija <a.dobija2245@gmail.com>
2026-06-12 19:58:06 +0200

    Create find by kod in plec

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/repository/PlecRepo.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/repository/PlecRepo.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.entity.Plec;
 
 public interface PlecRepo extends JpaRepository<Plec, Long> {
+    Plec findByKod(String kod);
 }
