bc86a9cb74a7a590a4299411bc8e69278638ca2d
1x4mil <kamilp200316@gmail.com>
2026-06-17 21:15:43 +0200

    Aktualizacja

--- src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/repository/WynikiRepo.java
+++ src/main/java/ubb/izi/skibs/IIb/SystemLabolatoriumDiagnostycznegoUBB/repository/WynikiRepo.java
@@ -4,13 +4,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ubb.izi.skibs.IIb.SystemLabolatoriumDiagnostycznegoUBB.entity.WynikBadania;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WynikiRepo
         extends JpaRepository<WynikBadania, Long> {
+
+    List<WynikBadania> findAllByOrderByDataWykonaniaDesc();
 
     List<WynikBadania>
     findByPacjentIdOrderByDataWykonaniaDesc(Long pacjentId);
 
     List<WynikBadania>
-    findAllByOrderByDataWykonaniaDesc();
+    findByPacjent_Uzytkownik_EmailOrderByDataWykonaniaDesc(
+            String email
+    );
+
+    Optional<WynikBadania>
+    findByIdAndPacjent_Uzytkownik_Email(
+            Long id,
+            String email
+    );
 }
